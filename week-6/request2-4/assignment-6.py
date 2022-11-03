839618f1dba4231e64735e9a476ea2c33602ea3e
PeiYingr <peggy2000000@gmail.com>
2022-11-04 01:27:05 +0800

    add week6 request2~4

---
+++ week-6/request2-4/assignment-6.py
@@ -0,0 +1,90 @@
+
+# 載入 Flask 所有相關的工具
+from flask import Flask # 載入 Flask
+from flask import request # 載入 Request 物件
+from flask import render_template # 載入 render_template 函式
+from flask import redirect
+from flask import url_for
+from flask import session # 載入 session 工具
+import mysql.connector
+import mysql.connector
+websitedb=mysql.connector.connect(user='root', 
+                                password='',
+                                host='127.0.0.1',
+                                database='website')
+cursor=websitedb.cursor()
+
+# 建立 Application 物件
+app=Flask(__name__,static_folder="assignment-6", static_url_path="/")
+# 設定 session 的密鑰
+app.secret_key=" any string but secret "  
+
+# 使用 GET 方法(沒有特別寫method)，處理路徑 / 的對應函式
+@app.route("/")
+def home():
+        return  render_template("home.html")
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    # 接收 POST 方法的 Query String
+    name=request.form["name"]
+    count=request.form["count"]
+    key=request.form["key"]
+
+    query=("SELECT username FROM member WHERE username=%s")
+    cursor.execute(query, (count,))
+    result = cursor.fetchone()
+    if result!=None:
+        return redirect("/error?message=帳號已經被註冊")
+    else:
+        add_member="INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
+        newdata=(name, count, key)
+        cursor.execute(add_member, newdata)
+        websitedb.commit()
+        return  render_template("home.html")
+
+@app.route("/signin",methods=["POST"])
+def signin():
+    # 接收 POST 方法的 Query String
+    count=request.form["count"]
+    key=request.form["key"]
+    # query 會以 tuple 形式呈現
+    query=("SELECT name, username, password, id FROM member WHERE username=%s")
+    cursor.execute(query, (count,))
+    result = cursor.fetchone()
+
+    if count=="" or key=="":
+        return redirect("/error?message=請輸入帳號、密碼")
+    elif count==result[1] and key==result[2]:
+        session["username"]=result[1]
+        session["name"]=result[0]
+        session["id"]=result[3]
+        return redirect("/member")
+    else:   
+        return redirect("/error?message=帳號或密碼輸入錯誤")
+
+@app.route("/member")
+def member():
+    if "username" not in session:
+           return redirect("/")
+    name=session["name"]
+    return render_template("member.html",nametext=name)
+
+@app.route("/error")
+def error():
+    text=request.args.get("message","")
+    return render_template("error.html",message=text)
+
+@app.route("/signout")
+def signout():
+    # 移除session中的username(count)
+    session.pop("username")
+    session.pop("name")
+    session.pop("id")
+    return redirect("/")
+
+# 啟動網站伺服器，可透過 port 參數指定埠號
+app.run(port=3000)
+
+cursor.close()
+websitedb.close()
